d319eadb63c3f9081c5fc9a5ba0de02bb64c2f7a
ratellll <latellbin@naver.com>
2022-06-08 19:22:45 +0900

    csv 읽기완료

---
+++ src/main/java/ReadTest3.java
@@ -0,0 +1,41 @@
+import java.awt.desktop.SystemEventListener;
+import java.io.*;
+import java.util.*;
+
+public class ReadTest3 {
+    public static void main(String[] args) {
+        ReadTest3 csvReader = new ReadTest3();
+        csvReader.readCSV();
+    }
+
+    public List<List<String>> readCSV() {
+        List<List<String>> csvList = new ArrayList<List<String>>();
+        File csv = new File("/Users/ratel/Desktop/study/JsonTest/src/top20.csv");
+        BufferedReader br = null;
+        String line = "";
+
+        try {
+            br = new BufferedReader(new FileReader(csv));
+            while ((line = br.readLine()) != null) { // readLine()은 파일에서 개행된 한 줄의 데이터를 읽어온다.
+                List<String> aLine = new ArrayList<String>();
+                String[] lineArr = line.split(","); // 파일의 한 줄을 ,로 나누어 배열에 저장 후 리스트로 변환한다.
+                aLine = Arrays.asList(lineArr);
+                csvList.add(aLine);
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) {
+                    br.close(); // 사용 후 BufferedReader를 닫아준다.
+                }
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return csvList;
+    }
+}
